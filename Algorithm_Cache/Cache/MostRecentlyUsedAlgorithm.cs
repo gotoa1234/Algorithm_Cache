8f959c70f122d280073a045ff42d02ec3d952377
gotoa1234 <cap8825@gmail.com>
2025-12-01 15:41:14 +0800

    update

--- Algorithm_Cache/Cache/MostRecentlyUsedAlgorithm.cs
+++ Algorithm_Cache/Cache/MostRecentlyUsedAlgorithm.cs
@@ -4,14 +4,30 @@
     {
         public void Execute()
         {
+            //Example A B C D E C D B:
         }
     }
 
     public class MostRecentlyUsedAlgorithm
     {
-        public MostRecentlyUsedAlgorithm()
+        private readonly int _capacity;
+        private readonly Dictionary<string, LinkedListNode<(string key, int value)>> _cache;
+        private readonly LinkedList<(string key, int value)> _links;
+
+        public MostRecentlyUsedAlgorithm(int capacity)
         {
             
         }
+
+        public int Get(int key)
+        {
+            return 0;
+        }
+
+
+        public void Put(int key, int value)
+        {
+        }
+
     }
 }
